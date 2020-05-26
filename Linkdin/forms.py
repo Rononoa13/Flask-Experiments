6bfc00bd6972ee8e1ca941b41945c158a0f86d77
Rononoa13 <sumitluitel42@gmail.com>
2020-05-26 19:49:49 +0545

    Add Validators to validate users input

--- Linkdin/forms.py
+++ Linkdin/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import Form
 from wtforms import  StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 class SignupForm(Form):
-    firstname = StringField('First Name')
-    lastname = StringField('Last Name')
-    email = StringField('Email')
-    password = PasswordField('Password')
-    submit = SubmitField('Sign Up')
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up', validators=[DataRequired()])
