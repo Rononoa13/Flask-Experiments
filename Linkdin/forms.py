62b40fdd864e4da3b0b61519f95d7ee11a6315a2
Rononoa13 <sumitluitel42@gmail.com>
2020-05-26 21:14:15 +0545

    Custom messages for Validators

--- Linkdin/forms.py
+++ Linkdin/forms.py
@@ -3,8 +3,8 @@ from wtforms import  StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 class SignupForm(Form):
-    firstname = StringField('First Name', validators=[DataRequired()])
-    lastname = StringField('Last Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    firstname = StringField('First Name', validators=[DataRequired("Please Enter your first name.")])
+    lastname = StringField('Last Name', validators=[DataRequired("Please Enter your last name.")])
+    email = StringField('Email', validators=[DataRequired("Please Enter your correct email.")])
+    password = PasswordField('Password', validators=[DataRequired("Please Enter your correct password.")])
     submit = SubmitField('Sign Up', validators=[DataRequired()])
